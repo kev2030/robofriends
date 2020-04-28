40acff8326c6957911e2385f85ad35b213d829d8
kev2030 <62866525+kev2030@users.noreply.github.com>
2020-04-27 21:38:32 -0600

    Updates

---
+++ precache-manifest.83fd6d0e63966fea0ac1c91b42b4d0a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77bf3001a74177961401d3d2cd32389f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "3e80248f8e33ae04399e",
+    "url": "/robofriends/static/css/main.4c5cd081.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/robofriends/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e80248f8e33ae04399e",
+    "url": "/robofriends/static/js/main.9bc92b8c.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
